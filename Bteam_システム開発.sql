de6fd55984785b067100610bd110b762646e811d
Kuwano02 <115756480+Kuwano02@users.noreply.github.com>
2022-11-15 12:12:21 +0900

    整理

--- Bteam_システム開発.sql
+++ Bteam_システム開発.sql
@@ -55,17 +55,17 @@ INSERT INTO users(user_id,user_name,user_pass,user_pc,user_address)VALUES("21011
 
 ２商品情報
 INSERT INTO shohins(shohin_id,shohin_name,shohin_price,picture_pass)VALUES("01","麻婆豆腐",1000,"mabo.jpg");
-INSERT INTO shohins(shohin_id,shohin_name,shohin_price,picture_pass)VALUES("01","春巻き",800,"harumaki.jpg");
-INSERT INTO shohins(shohin_id,shohin_name,shohin_price,picture_pass)VALUES("01","小籠包",800,"syouronpo.jpg");
-INSERT INTO shohins(shohin_id,shohin_name,shohin_price,picture_pass)VALUES("01","トッポギ",800,"toppogi.png");
-INSERT INTO shohins(shohin_id,shohin_name,shohin_price,picture_pass)VALUES("02","ビビンバ",900,"bibinba.png");
-INSERT INTO shohins(shohin_id,shohin_name,shohin_price,picture_pass)VALUES("03","サムギョプサル",1500,"samugyo.png");
-INSERT INTO shohins(shohin_id,shohin_name,shohin_price,picture_pass)VALUES("01","ピデ",550,"pide.png");
-INSERT INTO shohins(shohin_id,shohin_name,shohin_price,picture_pass)VALUES("02","トルコアイス",350,"torukoice.png");
-INSERT INTO shohins(shohin_id,shohin_name,shohin_price,picture_pass)VALUES("03","ケバブ",800,"kebabu.png");
-INSERT INTO shohins(shohin_id,shohin_name,shohin_price,picture_pass)VALUES("01","パエリア",1500,"paeria.png");
-INSERT INTO shohins(shohin_id,shohin_name,shohin_price,picture_pass)VALUES("01","トルティージャ",1000,"toruty.png");
-INSERT INTO shohins(shohin_id,shohin_name,shohin_price,picture_pass)VALUES("01","アヒージョ",1000,"ahi-jo.png");
+INSERT INTO shohins(shohin_id,shohin_name,shohin_price,picture_pass)VALUES("02","春巻き",800,"harumaki.jpg");
+INSERT INTO shohins(shohin_id,shohin_name,shohin_price,picture_pass)VALUES("03","小籠包",800,"syouronpo.jpg");
+INSERT INTO shohins(shohin_id,shohin_name,shohin_price,picture_pass)VALUES("04","トッポギ",800,"toppogi.png");
+INSERT INTO shohins(shohin_id,shohin_name,shohin_price,picture_pass)VALUES("05","ビビンバ",900,"bibinba.png");
+INSERT INTO shohins(shohin_id,shohin_name,shohin_price,picture_pass)VALUES("06","サムギョプサル",1500,"samugyo.png");
+INSERT INTO shohins(shohin_id,shohin_name,shohin_price,picture_pass)VALUES("07","ピデ",550,"pide.png");
+INSERT INTO shohins(shohin_id,shohin_name,shohin_price,picture_pass)VALUES("08","トルコアイス",350,"torukoice.png");
+INSERT INTO shohins(shohin_id,shohin_name,shohin_price,picture_pass)VALUES("09","ケバブ",800,"kebabu.png");
+INSERT INTO shohins(shohin_id,shohin_name,shohin_price,picture_pass)VALUES("10","パエリア",1500,"paeria.png");
+INSERT INTO shohins(shohin_id,shohin_name,shohin_price,picture_pass)VALUES("11","トルティージャ",1000,"toruty.png");
+INSERT INTO shohins(shohin_id,shohin_name,shohin_price,picture_pass)VALUES("12","アヒージョ",1000,"ahi-jo.png");
 
 -- アップデート文
 -- UPDATE users
