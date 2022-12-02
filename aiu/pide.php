ce3a6c31a82042da11780d22b7a43c17bc49295e
Kuwano02 <115756480+Kuwano02@users.noreply.github.com>
2022-12-02 12:17:39 +0900

    aiu 追加

---
+++ aiu/pide.php
@@ -0,0 +1,150 @@
+<?php
+require "component.php";
+  session_start();
+?>
+
+<!DOCTYPE html>
+<html>
+
+<head>
+  <link rel="stylesheet" href="style.css">
+<meta content="text/html; charset=utf-8" http-equiv="Content-Type">
+<title>商品詳細</title>
+<style>
+</style>
+<script type="text/javascript">
+</script>
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+<meta name="viewport" content="width=device-width, initial-scale=1" />
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css">
+<link href="style.css"rel="stylesheet"type="text/css">
+</head>
+<body>
+  <nav class="navbar navbar-expand-md navbar-dark" aria-label="Fourth navbar example" style="background-color: #FF9933;">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#"><img src="../img/rogo b t.png" width="20%"></a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarsExample04" aria-controls="navbarsExample04" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+      <div class="collapse navbar-collapse" id="navbarsExample04">
+        <ul class="navbar-nav me-auto mb-2 mb-md-0">
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="../webフロントエンド/catalog.html"><font size="5">トップ</font></a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#"><font size="5">お気に入り</font></a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#"><font size="5">ランキング</font></a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="../webフロントエンド/login.html"><font size="5">ログイン</font></a>
+          </li>
+        </ul>
+        <form role="search">
+          <input class="form-control" type="search" placeholder="レシピを検索" aria-label="Search">
+        </form>
+      </div>
+    </div>
+  </nav>
+  <br>
+
+      <div class="maindiv">
+        <div class="textimg">
+          <img src="../img/pide.png" width="420" height="350" class="rounded">
+        </div>
+          <div class="textimg text-align">
+            <h1>牛肉と野菜のピデ</h1>
+            <p>
+               <br>
+               ●ピデとは、フルンと言われる窯で焼き上げた直径30㎝位の円形でふっくらもちもちとした食感のトルコで欠かせないパンの一つです。<br>
+               ●ピデの生地には砂糖が使われており、ほのかに甘く懐かしい味がします。<br>
+               ●素朴な味で、出来立て熱々のピデは一口食べたらやめられない！
+               <br>
+            </p>
+
+
+            <div name="main" class="container">
+              <div class="row">
+
+                <div class="col-lg-1"></div>
+
+                <div class="col-lg-5">
+                  <div class="card">
+                    <div class="card-body">
+                      <h5 class="card-title text-warning">商品情報<br>—————————————————————</h5>
+                      <dl class="text-dark text-left">
+                        <dt>内容量</dt><dd>330ml ／ 本<br>※48本セットにてお届けします</dd>
+                        <dt>原材料名</dt><dd>強力・薄力粉,砂糖,塩,水,肉,玉ねぎ,トマト,チリパウダー</dd>
+                        <dt>保存方法</dt><dd>高温、直射日光を避けてください。</dd>
+                        <dt>原産国</dt><dd>トルコ</dd>
+                        <dt>輸入者の<br>名称及び住所</dt><dd>aso株式会社<br>東京都渋谷区恵比寿1-18-15</dd>
+                        <dt>賞味期限</dt><dd>2023年8月19日</dd>
+                      </dl>
+                    </div>
+                  </div>
+                </div>
+
+                <div class="col-lg-5">
+                  <div class="card">
+                      <div class="card-body">
+                      <h5 class="card-title text-warning">配送について<br>—————————————————————</h5>
+                      <dl class="text-dark text-left">
+                        <dt>送料</dt><dd>550円／件<br> ※送料は商品ごとにかかります。<br> ※配送先によって送料が異なる可能性があります。</dd>
+                        <dt>配送業者</dt><dd>佐川急便(沖縄県・離島のみヤマト運輸)</dd>
+                        <dt>配送可能地域</dt><dd>全国</dd>
+                      </dl>
+                      </div>
+                  </div>
+                </div>
+
+                <div class="row m-1 mt-4">
+                  <div class="col-md-4"></div>
+                  <!-- <div style="display: flex;">
+                    <div>{{ count }}</div>
+                    <button
+                        class="button"
+                        @click = "decrement"
+                    >-1</button>
+                    <button
+                        class="button"
+                        @click = "increment"
+                    >+1</button>
+                </div> -->
+                  <div class="col-md-4"></div>
+                </div>
+                <div class="row m-1 mt-3">
+                  <div class="col-2"></div>
+                  <div class="col-8">
+                    <div class="d-grid gap-2">
+                      <!-- <button class="btn btn-warning btn-lg text-white" type="button">カートに入れる</button><br> -->
+<!-- 中の数字変 -->
+            <form action="cart.php" class="mt-3" method="post">
+              <input type="hidden" name="product_name" value="<?= $product[0]["product_name"] ?>">
+              <select name="num">
+                  <option value=1>1</option> 
+                  <option value=2>2</option>
+                  <option value=3>3</option> 
+                  <option value=4>4</option>
+                  <option value=5>5</option>
+              </select>
+              <!-- <button type="submit" class="btn btn-primary"><a href="./shoping.php">カート</a></button>  -->
+              <button type="submit" class="btn btn-primary">カゴに入れる</button> 
+          </form> 
+        </div> 
+     
+                      <!-- <a class="btn btn-Light btn-lg text-dark" href="../webフロントエンド/catalog.html" role="button">戻る</a> -->
+                    </div>
+                  </div>
+                  <div class="col-2"></div>
+                </div>
+              </div>
+            </div>
+          </div>
+      </div>
+      <!-- <script src="https://cdn.jsdelivr.net/npm/vue@2.6.14/dist/vue.js"></script>
+    <script src="./script.js"></script> -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+
+</body>
---
+++ aiu/shoping.php
@@ -0,0 +1,116 @@
+<?php
+  require "component.php";
+  session_start();
+?>
+<!doctype html>
+<html lang="ja">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <title>Hell,shoping!</title>
+    <style>
+        .bg-img{
+        background-image: url("../img/topimage 2.jpg");
+        background-size: cover;
+        background-position: center 60%;
+        }
+    </style>
+    
+  </head>
+  <body>
+
+  <nav class="navbar navbar-expand-md navbar-dark" aria-label="Fourth navbar example" style="background-color: #FF9933;">
+		<div class="container-fluid">
+		  <a class="navbar-brand" href="#"><img src="../img/rogo_u.png" width="20%"></a>
+		  <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarsExample04" aria-controls="navbarsExample04" aria-expanded="false" aria-label="Toggle navigation">
+			<span class="navbar-toggler-icon"></span>
+		  </button>
+	
+		  <div class="collapse navbar-collapse" id="navbarsExample04">
+			<ul class="navbar-nav me-auto mb-2 mb-md-0">
+			  <li class="nav-item">
+				<a class="nav-link active" aria-current="page" href="#"><font size="5">トップ</font></a>
+			  </li>
+			  <li class="nav-item">
+				<a class="nav-link" href="#"><font size="5">お気に入り</font></a>
+			  </li>
+			</ul>
+			<form role="search">
+			  <input class="form-control" type="search" placeholder="レシピを検索" aria-label="Search">
+			</form>
+		  </div>
+		</div>
+	  </nav>
+<div name="maindiv" class="container-fluids">
+	<div class="bg-img p-5">
+		<div class="container">
+			<img src="../img/rogo b t.png" class="img-fluid" width="30%">
+			<p class="text-white text-center"><br></p>
+		</div>
+	</div>
+	<div class="row mt-2 mb-1">
+		<div class="col-12"  style="background-color:#f7a10c;">
+			<h2 class="text-center text-white p-1"><i class="bi bi-hand-thumbs-up"></i>おすすめのメニュー</h2>
+		</div>
+	</div>
+
+    <div class="couneainer text-center">
+    <h1>世界の料理</h1>
+
+<!-- 一覧画面からカートに商品が入っているか確認できる　別にいらん -->
+    <strong class="h6 text-danger"><a href="cart.php" class="text-danger"> 買い物かご</a>は、
+        <?php
+            if( isset($_SESSION[ "cart"]) && count($_SESSION["cart"])>0){          
+              echo count($_SESSION[ "cart"]);
+              echo "商品が入っています。"; 
+            }else{
+              echo "空です。";
+            }
+          ?>   
+      </strong> 
+<!-- 画像　名前　値段出力 -->
+<div class="row">
+      <?php 
+      for($i=0;$i<8;$i++){
+          ?> 
+          <div class="col-md-3">
+            <?php 
+            echo "<img src='".$product[$i]["img"]."' class='img-fluid rounded'>";
+            echo "<h3>".$product[$i]["product_name"]."</h3>";
+            echo "<span class='text-danger'>".number_format($product[$i]["price"])."円</span><br>";
+
+
+            echo "<form>";
+            foreach ($arr as $tmp) printf('<button formaction = "%s">%s</button>',$tmp['url'],
+            $tmp['name']);
+            echo "</form>"; 
+            ?>   
+            <!-- <div class="col-md-3">
+            <a href="pide.php">商品詳細</a>
+            </div>   -->
+
+        </div>
+      <?php
+      }
+      ?>
+
+      
+
+
+
+<!-- <div class="col-md-3">
+<button type="submit" class="btn btn-primary">商　品　詳　細</button>
+</div>
+<div class="col-md-3">
+<button type="submit" class="btn btn-primary">商　品　詳　細</button>
+</div>
+<div class="col-md-3">
+<button type="submit" class="btn btn-primary">商　品　詳　細</button>
+</div> -->
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    </div>
+  </body>
+</html>
