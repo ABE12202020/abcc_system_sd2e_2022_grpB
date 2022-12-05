7ca38d8c21c6752325ca35bfff3bfe0f3a9a3035
Kuwano02 <115756480+Kuwano02@users.noreply.github.com>
2022-12-05 13:01:54 +0900

    あいう改

--- aiu/component.php
+++ aiu/component.php
@@ -4,54 +4,55 @@ $product = [
         "product_name" => "ピデ",
         "price" => 550 ,
         "img" => "../img/pide.png",
-        // "pinfo" => "pide.php"
+         "url" => "https://www.google.com/?hl=ja"
+        //  "url" => "http://localhost/web/aiu/pide.php"
     ],
     [    
         "product_name" => "アヒージョ",
         "price" => 550 ,
-        "img" => "../img/ahi-jo.png"
+        "img" => "../img/ahi-jo.png",
+        "url" => "https://www.google.com/?hl=ja"
     ],
     [    
         "product_name" => "パエリア",
         "price" => 2480 ,
-        "img" => "../img/paeria.png"
+        "img" => "../img/paeria.png",
+        "url" => "https://www.google.com/?hl=ja"
     ],
     [    
         "product_name" => "トルコアイス",
         "price" => 2480 ,
-        "img" => "../img/torukoice.png"
+        "img" => "../img/torukoice.png",
+        "url" => "https://www.google.com/?hl=ja"
     ],[    
         "product_name" => "トッポギ",
         "price" => 2480 ,
-        "img" => "../img/toppogi.png"
+        "img" => "../img/toppogi.png",
+        "url" => "https://www.google.com/?hl=ja"
     ],
     [    
         "product_name" => "サムギョプサル",
         "price" => 2480 ,
-        "img" => "../img/samugyo.png"
+        "img" => "../img/samugyo.png",
+        "url" => "https://www.google.com/?hl=ja"
     ],
     [    
         "product_name" => "ケバブ",
         "price" => 2480 ,
-        "img" => "../img/kebabu.png"
+        "img" => "../img/kebabu.png",
+        "url" => "https://www.google.com/?hl=ja"
     ],
     [    
         "product_name" => "ビビンバ",
         "price" => 2480 ,
-        "img" => "../img/bibinba.png"
+        "img" => "../img/bibinba.png",
+        "url" => "https://www.google.com/?hl=ja"
     ]
 
 ];
 ?>
 
-<?php
 
-$arr = [
-    ['name' => 'pide','url' => 'https://www.google.com/?hl=ja'],
-    ['name' => 'pide','url' => 'https://paiza.jp/works/mypage'],
-    ['name' => 'pide','url' => 'https://www.google.com/?hl=ja'],
-];
-?>
 
 
 
