d4737d51851432245c2b2a86144c4cc66cac6936
Kuwano02 <115756480+Kuwano02@users.noreply.github.com>
2022-12-13 10:39:21 +0900

    a

--- cd/d.php
+++ cd/d.php
@@ -20,7 +20,7 @@
       <div class="collapse navbar-collapse" id="navbarsExample04">
         <ul class="navbar-nav me-auto mb-2 mb-md-0">
           <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="../webフロントエンド/catalog.html"><font size="5">トップ</font></a>
+            <!-- <a class="nav-link active" aria-current="page" href=""><font size="5">トップ</font></a> -->
           </li>
           <li class="nav-item">
             <a class="nav-link" href="login_complete.html"><font size="5">ログアウト</font></a>
